864cb454621ed7d7d5dd52e1058ed9f0df1c17c4
Bernhard Urban <bernhard.urban@xamarin.com>
2017-05-10 01:48:20 +0200

    icall stuff is needed in interpreter

--- sdks/build-interp.sh
+++ sdks/build-interp.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 
-MONO_PATH=../../out/bcl/monotouch ./mono/mini/mono-sgen "--aot=mtriple=aarch64-apple-darwin10,interp,asmonly" ../../out/bcl/monotouch/mscorlib.dll/
+MONO_PATH=../../out/bcl/monotouch ./mono/mini/mono-sgen "--aot=mtriple=aarch64-apple-darwin10,interp,asmonly,static" ../../out/bcl/monotouch/mscorlib.dll/
 
 as -arch arm64 -miphoneos-version-min=8.0 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk /Users/bernhardu/work/mono-sdks/out/bcl/monotouch/mscorlib.dll.s -o /Users/bernhardu/work/mono-sdks/out/bcl/monotouch/mscorlib.dll.o
 ld -dylib -arch arm64 -ios_version_min 8.0  /Users/bernhardu/work/mono-sdks/out/bcl/monotouch/mscorlib.dll.o -o  /Users/bernhardu/work/mono-sdks/out/bcl/monotouch/mscorlib.dll.so
+
+
+install_name_tool -id '@rpath/libMonoPosixHelper.dylib' libMonoPosixHelper.dylib
+
+# verify with
+otool -L libMonoPosixHelper.dylib
--- sdks/ios.mk
+++ sdks/ios.mk
@@ -48,8 +48,6 @@ ARMV7_CONFIGURE_FLAGS = \
 	--without-ikvm-native \
 	--with-tls=pthread \
 	--without-sigaltstack \
-	--enable-icall-export \
-	--disable-icall-tables \
 	--disable-executables \
 	--disable-nls \
 	--disable-iconv \
@@ -76,7 +74,7 @@ ARM64_AC_VARS=mono_cv_uscore=yes mono_cv_sizeof_sunpath=104 ac_cv_func_posix_get
 
 ARM64_TARGET_CPPFLAGS=-DSMALL_CONFIG -DDISABLE_POLICY_EVIDENCE=1 -DDISABLE_PROCESS_HANDLING=1 -D_XOPEN_SOURCE -DMONOTOUCH=1 -DHOST_IOS -DHAVE_LARGE_FILE_SUPPORT=1 -Wl,-application_extension -miphoneos-version-min=$(IOS_MIN_VERSION) -isysroot $(XCODE_DEVELOPER_ROOT)/Platforms/iPhoneOS.platform/Developer/SDKs/$(IPHONEOS_VERSION) -arch arm64
 
-ARM64_TARGET_CFLAGS=-O2 -gdwarf-2 -DSMALL_CONFIG -DDISABLE_POLICY_EVIDENCE=1 -DDISABLE_PROCESS_HANDLING=1 -D_XOPEN_SOURCE -DMONOTOUCH=1 -DHOST_IOS -DHAVE_LARGE_FILE_SUPPORT=1 -Wl,-application_extension -miphoneos-version-min=$(IOS_MIN_VERSION) -isysroot $(XCODE_DEVELOPER_ROOT)/Platforms/iPhoneOS.platform/Developer/SDKs/$(IPHONEOS_VERSION) -fexceptions $(ARM64_BITCODE_MARKER)
+ARM64_TARGET_CFLAGS=-O0 -gdwarf-2 -DSMALL_CONFIG -DDISABLE_POLICY_EVIDENCE=1 -DDISABLE_PROCESS_HANDLING=1 -D_XOPEN_SOURCE -DMONOTOUCH=1 -DHOST_IOS -DHAVE_LARGE_FILE_SUPPORT=1 -Wl,-application_extension -miphoneos-version-min=$(IOS_MIN_VERSION) -isysroot $(XCODE_DEVELOPER_ROOT)/Platforms/iPhoneOS.platform/Developer/SDKs/$(IPHONEOS_VERSION) -fexceptions $(ARM64_BITCODE_MARKER)
 
 ARM64_TARGET_CXXFLAGS=-DSMALL_CONFIG -DDISABLE_POLICY_EVIDENCE=1 -DDISABLE_PROCESS_HANDLING=1 -D_XOPEN_SOURCE -DMONOTOUCH=1 -DHOST_IOS -DHAVE_LARGE_FILE_SUPPORT=1 -Wl,-application_extension -miphoneos-version-min=$(IOS_MIN_VERSION) -isysroot $(XCODE_DEVELOPER_ROOT)/Platforms/iPhoneOS.platform/Developer/SDKs/$(IPHONEOS_VERSION) -arch arm64 -fexceptions $(ARM64_BITCODE_MARKER)
 
@@ -110,8 +108,6 @@ ARM64_CONFIGURE_FLAGS = \
 	--without-ikvm-native \
 	--with-tls=pthread \
 	--without-sigaltstack \
-	--enable-icall-export \
-	--disable-icall-tables \
 	--disable-executables \
 	--disable-nls \
 	--disable-iconv \

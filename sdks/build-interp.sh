120290dd4536832b6d60015fb8fd3c5628fee789
Bernhard Urban <bernhard.urban@xamarin.com>
2017-05-10 16:51:30 +0200

    update deployment notes for minecraft with interp

--- sdks/build-interp.sh
+++ sdks/build-interp.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+make -C sdks -f ios.mk configure-ios-cross64 && make -C sdks -f ios.mk ios-cross64/aarch64-apple-darwin10.h
 
-MONO_PATH=../../out/bcl/monotouch ./mono/mini/mono-sgen "--aot=mtriple=aarch64-apple-darwin10,interp,asmonly,static" ../../out/bcl/monotouch/mscorlib.dll/
-
-as -arch arm64 -miphoneos-version-min=8.0 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk /Users/bernhardu/work/mono-sdks/out/bcl/monotouch/mscorlib.dll.s -o /Users/bernhardu/work/mono-sdks/out/bcl/monotouch/mscorlib.dll.o
-ld -dylib -arch arm64 -ios_version_min 8.0  /Users/bernhardu/work/mono-sdks/out/bcl/monotouch/mscorlib.dll.o -o  /Users/bernhardu/work/mono-sdks/out/bcl/monotouch/mscorlib.dll.so
+make -C sdks/ios-cross64 -j8
+make -C sdks/ios-arm64 -j8
 
 
-install_name_tool -id '@rpath/libMonoPosixHelper.dylib' libMonoPosixHelper.dylib
+cd sdks/ios-cross64
 
-# verify with
-otool -L libMonoPosixHelper.dylib
+# dirty boy: use mscorlib.dll from desktop profile;  monotouch disables reflection_emit in BCL profile.
+MONO_PATH=$minecraftpe/handheld/lib/mono/lib_shared  ./mono/mini/mono-sgen "--aot=mtriple=aarch64-apple-darwin10,interp,asmonly,static" $minecraftpe/handheld/lib/mono/lib_shared/mscorlib.dll
+
+as -arch arm64 -miphoneos-version-min=8.0 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk -o $minecraftpe/handheld/lib/mono/lib_shared/mscorlib.dll.{o,s}
+
+cd -
+
+cd sdks/ios-arm64
+cp mono/mini/.libs/libmonosgen-2.0.a $minecraftpe/handheld/lib/mono/lib_ios/aarch64
+cp support/.libs/libMonoPosixHelper.a $minecraftpe/handheld/lib/mono/lib_ios/aarch64
+
+cp support/.libs/libMonoPosixHelper.dylib $minecraftpe/handheld/lib/mono/lib_ios/aarch64
+install_name_tool -id '@executable_path/Frameworks/libMonoPosixHelper.dylib' $minecraftpe/handheld/lib/mono/lib_ios/aarch64/libMonoPosixHelper.dylib
+otool -L $minecraftpe/handheld/lib/mono/lib_ios/aarch64/libMonoPosixHelper.dylib
